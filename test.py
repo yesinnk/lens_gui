c4c6d317c3ef58283fefe4412a76de3dafe44e71
Weiyu Li <weiyu.li@mail.utoronto.ca>
2020-05-21 08:52:41 -0400

    update

--- test.py
+++ test.py
@@ -1,15 +1,25 @@
 import PySimpleGUI as sg
 
-menu_def = [['File', ['Open', 'Save', 'Exit', 'Properties']],
-            ['Edit', ['Paste', ['Special', 'Normal', ], 'Undo'], ],
-            ['Help', 'About...'], ]
-
 layout = [
-    [sg.Menu(menu_def, tearoff=True,font = ("Helvetica", 20))],
-    [sg.Text('All graphic widgets in one window!', size=(30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)]
+    [
+        sg.Graph(
+            canvas_size=(400, 400),
+            graph_bottom_left=(0, 0),
+            graph_top_right=(400, 400),
+            key="graph"
+        )
+    ]
 ]
 
+window = sg.Window("rect on image", layout)
+window.Finalize()
 
-window = sg.Window('Everything bagel', layout, default_element_size=(40, 1), grab_anywhere=False)
+graph = window.Element("graph")
 
-event, values = window.read()
+# graph.DrawImage(filename="foo.png", location=(0, 400))
+graph.DrawRectangle((200, 200), (250, 300), line_color="red")
+
+while True:
+    event, values = window.Read()
+    if event is None:
+        break
--- unit_viewer.py
+++ unit_viewer.py
@@ -1,12 +1,27 @@
 import PySimpleGUI as sg
-BUTTONSIZE = (30,6)
-BUTTONFONT = ("Helvetica", 20)
-SMALLERFONT = ("Helvetica", 20)
+TOPFONT = ("Helvetica", 15)
+FRAMEFONT = ("Helvetica", 15)
+LISTFONT = ("Helvetica", 20)
+BLANKFONT = 'any 20'
+
+LISTENTRYSIZE = (23, 7)
 SQUAREBOX = (2,10)
 RECTANGULARBOX = (4,10)
+CANVASWIDTH = 250
+CAMVASHEIGHT = 235
 ENABLEEVENTS = True
 SUBMITCHAMGE = True
+SQUARESIZE = 25
+
+# variables from network
 ALLTRAINING = ('0','1','2','3','4','5','6')
+bias_layer = ["red"]
+input_layer = ["blue","red"]
+hidden_layer = ["blue","brown"]
+output_layer = ["black"]
+output_frame = ["red"]
+
+
 
 menu_def = [['&Viewer', ['&Update After',['Examples','Weight Updates','Progress Reports','Training and Testing','Never'],
                         '&Cell Size',['5','6','7','8','9','10','11','12','14','16','18','20','24','28','32','40'],
@@ -16,24 +31,27 @@ menu_def = [['&Viewer', ['&Update After',['Examples','Weight Updates','Progress 
             ['&Procedure',['Test','Train']],
             ['&Value', ['Outputs and Targets', 'Outputs', 'Targets', 'Inputs', 'External Inputs', 'Output Derivatives',
                        'Input Derivatives', 'Gains', 'Link Weights', 'Link Derivs', 'Link Deltas']],
-['&Palette',['Blue-Black-Red','Blue-Red-Yellow','Black-Gray-White','Hinton Diagram']]]
+            ['&Palette',['Blue-Black-Red','Blue-Red-Yellow','Black-Gray-White','Hinton Diagram']]]
 
 
+layout = [  [sg.Menu(menu_def, tearoff=True,font=TOPFONT)],
 
+             [sg.Text('Event   ',font=TOPFONT), sg.Text('Example Time   ',font=TOPFONT),
+              sg.Text('Event Time',font=TOPFONT),sg.Input(' ',size = (8,10), font = 'Any 20')],
 
-layout = [  [sg.Menu(menu_def, tearoff=True,font=BUTTONFONT,size=(40,100))],
-
-             [sg.Text('Event   ',font=SMALLERFONT), sg.Text('Example Time   ',font=SMALLERFONT),
-              sg.Text('Event Time',font=SMALLERFONT),sg.Input(' ',size = (8,10), font = 'Any 20')],
-
-
-             [sg.Input(' ',size = SQUAREBOX, font = 'Any 20'),sg.Text('/',font = 'Any 20'),sg.Input(' ',size = SQUAREBOX, font = 'Any 20'),sg.T('', font='any 20'),
-              sg.Input(' ',size = RECTANGULARBOX, font = 'Any 20'),sg.Text('/',font = 'Any 20'),sg.Input(' ',size = RECTANGULARBOX, font = 'Any 20'),sg.T('   ', font='any 20'),
-              sg.Input(' ',size = RECTANGULARBOX, font = 'Any 20'),sg.Text('/',font = 'Any 20'),sg.Input(' ',size = RECTANGULARBOX, font = 'Any 20'),
+             [sg.Input(' ',size = SQUAREBOX, font = BLANKFONT),sg.Text('/',font = BLANKFONT),sg.Input(' ',size = SQUAREBOX, font = BLANKFONT),sg.T('', font=BLANKFONT),
+              sg.Input(' ',size = RECTANGULARBOX, font = BLANKFONT),sg.Text('/',font = BLANKFONT),sg.Input(' ',size = RECTANGULARBOX, font = BLANKFONT),sg.T('   ', font=BLANKFONT),
+              sg.Input(' ',size = RECTANGULARBOX, font = BLANKFONT),sg.Text('/',font = BLANKFONT),sg.Input(' ',size = RECTANGULARBOX, font = BLANKFONT),
               sg.Input(' ',size = (8,10), font = 'Any 20'), sg.Slider(range=(1, 100), orientation='h', size=(34, 20), default_value=85)],
 
-            [sg.Frame('Training Set', [[sg.Listbox(values=ALLTRAINING, size=(23, 7),font=("Helvetica", 20))]],font=("Helvetica", 20)),
-            sg.Frame('Outputs and Targets',[[sg.Listbox(values=ALLTRAINING, size=(23, 7),font=("Helvetica", 20))]], font=("Helvetica", 20)),
+            [sg.Frame('Training Set', [[sg.Listbox(values=ALLTRAINING, size=LISTENTRYSIZE,font=LISTFONT)]],font=FRAMEFONT),
+            sg.Frame('Outputs and Targets',[[
+                                                    sg.Graph(
+                                                        canvas_size=(CANVASWIDTH, CAMVASHEIGHT),
+                                                        graph_bottom_left=(0, 0),
+                                                        graph_top_right=(CANVASWIDTH, CAMVASHEIGHT),
+                                                        key="graph"
+                                                        )]]),
             sg.Slider(range=(1, 100), orientation='v', size=(11, 20), default_value=50)]
         ]
 
@@ -42,6 +60,45 @@ window = sg.Window('unit viewer',
                    border_depth=0,
                    use_default_focus=False,
                    )
+
+# for the graph
+window.Finalize()
+graph = window.Element("graph")
+#
+# BIASLAYER = ["red"]
+# INPUTLAER = ["blue","red"]
+# HIDDENLAYER = ["blue","brown"]
+# OUTPUTLAYER = ["black"]
+# OUTPUTFRAME = ["red"]
+rec_height = CAMVASHEIGHT/9
+bias_v_coord = rec_height
+input_v_coord = rec_height*3
+hidden_v_coord = rec_height*5
+output_v_coord = rec_height*7
+bias_h_coord = [CANVASWIDTH/2-SQUARESIZE]
+input_h_coord = [CANVASWIDTH/2-SQUARESIZE,CANVASWIDTH/2]
+hidden_h_coord = [CANVASWIDTH/2-SQUARESIZE,CANVASWIDTH/2]
+output_h_coord = [CANVASWIDTH/2]
+
+for i in range(len(bias_layer)):
+    color = bias_layer[i]
+    h_coord = bias_h_coord[i]
+    graph.DrawRectangle((h_coord, bias_v_coord), (h_coord+SQUARESIZE, bias_v_coord+SQUARESIZE), fill_color=color)
+
+for i in range(len(input_layer)):
+    color = input_layer[i]
+    h_coord = input_h_coord[i]
+    graph.DrawRectangle((h_coord, input_v_coord), (h_coord+SQUARESIZE, input_v_coord+SQUARESIZE), fill_color=color)
+
+for i in range(len(hidden_layer)):
+    color = hidden_layer[i]
+    h_coord = hidden_h_coord[i]
+    graph.DrawRectangle((h_coord, hidden_v_coord), (h_coord+SQUARESIZE, hidden_v_coord+SQUARESIZE), fill_color=color)
+
+for i in range(len(output_layer)):
+    color = output_layer[i]
+    h_coord = output_h_coord[i]
+    graph.DrawRectangle((h_coord, output_v_coord), (h_coord+SQUARESIZE, output_v_coord+SQUARESIZE), fill_color=color)
 
 button, value = window.Read()
 
