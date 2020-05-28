bef195936a1c375ba711a5d9ab0fac2856a6cb5d
Yesinn Kwak <yesinn.kwak@gmail.com>
2020-05-28 23:53:50 +0900

    network init

---
+++ network.py
@@ -0,0 +1,18 @@
+class Network:
+    def __init__(self,name,num,numUnits,numInputs,numOutputs
+                numLinks,trainingSet,testingSet):
+        self.name = name
+        self.num = num
+        self.numUnits = numUnits
+        self.numInputs = numOutputs
+        self.numLinks = numLinks
+        self.trainingSet = trainingSet
+        self.testingSet = testingSet
+
+class Unit:
+    def __init__():
+
+
+class Block:
+    def __init__():
+        
